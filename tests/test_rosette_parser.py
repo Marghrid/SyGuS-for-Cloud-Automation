0ab1c7c22f70c32dadde5d6aefeb47241eb292d7
Marghrid <margarida@cmu.edu>
2023-10-17 17:36:30 -0400

    Test cases for 'synthesize_data_transforms'

--- tests/test_rosette_parser.py
+++ tests/test_rosette_parser.py
@@ -1,6 +1,6 @@
 import unittest
 
-from synthesizer.to_rosette import parse_rosette_output
+from synthesizer.to_rosette import convert_rosette_to_jsonpath, parse_rosette_output
 
 
 class RosetteParserTests(unittest.TestCase):
@@ -12,3 +12,11 @@ class RosetteParserTests(unittest.TestCase):
     def test2(self):
         self.assertEqual(parse_rosette_output("(define (f?_2 x) (descendant (index x 0) \"Item\"))"),
                          ('descendant', (('index', ('x', 0)), "Item")))
+
+    def test3(self):
+        self.assertEqual(convert_rosette_to_jsonpath("(define (f?_11 x) (child (index x 0) \"Item\"))"), '$[0].Item')
+
+    def test4(self):
+        self.assertEqual(
+            convert_rosette_to_jsonpath('(define (f$_4 x) (syntEq (index (descendant (index x 1) "Name") 0) "stopping"))'),
+            '$[1]..Name[0] == "stopping"')
---
+++ tests/test_synthesizer.py
@@ -0,0 +1,47 @@
+import unittest
+
+from synthesizer.data_transforms_synthesizer import synthesize_data_transforms
+
+
+class RosetteParserTests(unittest.TestCase):
+
+    def test1(self):
+        example = {"name": "f0",
+                   "constraints": [
+                       {"inputs": ['a', 'b', 'c'], "output": "c"},
+                       {"inputs": ['e', 'f', 'd'], "output": "d"},
+                       {"inputs": ['v', 'x', 'f'], "output": "f"},
+                       {"inputs": ['hello', ':)', 'n'], "output": 'n'},
+                   ]}
+        result = synthesize_data_transforms("test", [example], 60, False)
+        self.assertEqual(result[0]['solution'], '$[2]')
+
+    def test2(self):
+        example = {"name": "f0",
+                   "constraints": [
+                       {"inputs": [{'a': 4, 'b': 5, 'c': 6}], "output": 4},
+                       {"inputs": [{'e': 5, 'a': 5, 'd': 6}], "output": 5},
+                       {"inputs": [{'v': 4, 'x': 3, 'a': 7}], "output": 7},
+                   ]}
+        result = synthesize_data_transforms("test", [example], 60, False)
+        self.assertEqual(result[0]['solution'], '$[0].a')
+
+    def test3(self):
+        example = {"name": "f0",
+                   "constraints": [
+                       {"inputs": [{'a': 4, 'b': 5, 'c': 6}], "output": 4},
+                       {"inputs": [{'e': 5, 'a': 5, 'd': 6}], "output": 4},
+                       {"inputs": [{'v': 4, 'x': 3, 'a': 7}], "output": 7},
+                   ]}
+        result = synthesize_data_transforms("test", [example], 60, False)
+        self.assertEqual(result[0]['solution'], '(unsat)')
+
+    def test4(self):
+        example = {"name": "f0",
+                   "constraints": [
+                       {"inputs": [{'lvl0': {'a': 4, 'b': 5, 'c': 6}}], "output": 4},
+                       {"inputs": [{'e': 5, 'a': 5, 'd': 6}], "output": 5},
+                       {"inputs": [{'v': 4, 'x': 3, 'a': 7}], "output": 7},
+                   ]}
+        result = synthesize_data_transforms("test", [example], 60, False)
+        self.assertEqual(result[0]['solution'], '$[0]..a[0]')
