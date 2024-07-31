39257df8c507b3efee656813c0fe45d4358eaa26
Marghrid <margarida@cmu.edu>
2024-07-31 12:41:52 -0400

    Fix tests to work for new Encoder classes

--- tests/test_rosette_parser.py
+++ tests/test_rosette_parser.py
@@ -1,23 +1,27 @@
 import unittest
 
-from synthesizer.encoder.json_to_rosette import convert_rosette_to_jsonpath, parse_rosette_output
+from synthesizer.encoder.json_to_rosette import Json2RosetteEncoder
 
 
 class RosetteParserTests(unittest.TestCase):
 
     def test1(self):
-        self.assertEqual(parse_rosette_output("(define (f?_11 x) (child (index x 0) \"Item\"))"),
+        encoder = Json2RosetteEncoder()
+        self.assertEqual(encoder.parse_rosette_output("(define (f?_11 x) (child (index x 0) \"Item\"))"),
                          ('child', (('index', ('x', 0)), "Item")))
 
     def test2(self):
-        self.assertEqual(parse_rosette_output("(define (f?_2 x) (descendant (index x 0) \"Item\"))"),
-                         ('descendant', (('index', ('x', 0)), "Item")))
+        encoder = Json2RosetteEncoder()
+        out = encoder.parse_rosette_output("(define (f?_2 x) (descendant (index x 0) \"Item\"))")
+        self.assertEqual(out, ('descendant', (('index', ('x', 0)), "Item")))
 
     def test3(self):
-        self.assertEqual(convert_rosette_to_jsonpath("(define (f?_11 x) (child (index x 0) \"Item\"))"), '$[0].Item')
+        encoder = Json2RosetteEncoder()
+        out = encoder.convert_rosette_to_jsonpath("(define (f?_11 x) (child (index x 0) \"Item\"))")
+        self.assertEqual(out, '$[0].Item')
 
     def test4(self):
-        self.assertEqual(
-            convert_rosette_to_jsonpath(
-                '(define (f$_4 x) (syntEq (index (descendant (index x 1) "Name") 0) "stopping"))'),
-            '($[1]..Name[0] == "stopping")')
+        encoder = Json2RosetteEncoder()
+        out = encoder.convert_rosette_to_jsonpath(
+            '(define (f$_4 x) (syntEq (index (descendant (index x 1) "Name") 0) "stopping"))')
+        self.assertEqual(out, '($[1]..Name[0] == "stopping")')
--- tests/test_synthesizer.py
+++ tests/test_synthesizer.py
@@ -2,11 +2,12 @@ import sys
 import unittest
 
 from synthesizer.data_transforms_synthesizer import synthesize_data_transforms
+from synthesizer.util import SynthesisSolver
 
 
-class RosetteParserTests(unittest.TestCase):
+class JsonSynthesisTests(unittest.TestCase):
 
-    def test1(self):
+    def test1cvc5(self):
         example = {"name": "f0",
                    "constraints": [
                        {"inputs": ['a', 'b', 'c'], "output": "c"},
@@ -15,38 +16,87 @@ class RosetteParserTests(unittest.TestCase):
                        {"inputs": ['hello', ':)', 'n'], "output": 'n'},
                    ]}
         result = synthesize_data_transforms(
-            sys._getframe().f_code.co_name, [example], 60, False)
+            sys._getframe().f_code.co_name, [example], SynthesisSolver.CVC5, 60, False)
         self.assertEqual(result[0]['solution'], '$[2]')
 
-    def test2(self):
+    def test1rosette(self):
+        example = {"name": "f0",
+                   "constraints": [
+                       {"inputs": ['a', 'b', 'c'], "output": "c"},
+                       {"inputs": ['e', 'f', 'd'], "output": "d"},
+                       {"inputs": ['v', 'x', 'f'], "output": "f"},
+                       {"inputs": ['hello', ':)', 'n'], "output": 'n'},
+                   ]}
+        result = synthesize_data_transforms(
+            sys._getframe().f_code.co_name, [example], SynthesisSolver.Rosette, 60, False)
+        self.assertEqual(result[0]['solution'], '$[2]')
+
+    def test2cvc5(self):
         example = {"name": "f0",
                    "constraints": [
                        {"inputs": [{'a': 4, 'b': 5, 'c': 6}], "output": 4},
                        {"inputs": [{'e': 5, 'a': 5, 'd': 6}], "output": 5},
                        {"inputs": [{'v': 4, 'x': 3, 'a': 7}], "output": 7},
                    ]}
-        result = synthesize_data_transforms(sys._getframe().f_code.co_name, [example], 60,
-                                            False)
+        result = synthesize_data_transforms(
+            sys._getframe().f_code.co_name, [example], SynthesisSolver.CVC5, 60,
+            False)
         self.assertEqual(result[0]['solution'], '$[0].a')
 
-    def test3(self):
+    def test2rosette(self):
+        example = {"name": "f0",
+                   "constraints": [
+                       {"inputs": [{'a': 4, 'b': 5, 'c': 6}], "output": 4},
+                       {"inputs": [{'e': 5, 'a': 5, 'd': 6}], "output": 5},
+                       {"inputs": [{'v': 4, 'x': 3, 'a': 7}], "output": 7},
+                   ]}
+        result = synthesize_data_transforms(
+            sys._getframe().f_code.co_name, [example], SynthesisSolver.Rosette, 60,
+            False)
+        self.assertEqual(result[0]['solution'], '$[0].a')
+
+    def test3cvc5(self):
         example = {"name": "f0",
                    "constraints": [
                        {"inputs": [{'a': 4, 'b': 5, 'c': 6}], "output": 4},
                        {"inputs": [{'e': 5, 'a': 5, 'd': 6}], "output": 4},
                        {"inputs": [{'v': 4, 'x': 3, 'a': 7}], "output": 7},
                    ]}
-        result = synthesize_data_transforms(sys._getframe().f_code.co_name, [example], 60,
-                                            False)
+        result = synthesize_data_transforms(
+            sys._getframe().f_code.co_name, [example], SynthesisSolver.CVC5, 60,
+            False)
+        self.assertEqual('(unsat)', result[0]['solution'])
+
+    def test3rosette(self):
+        example = {"name": "f0",
+                   "constraints": [
+                       {"inputs": [{'a': 4, 'b': 5, 'c': 6}], "output": 4},
+                       {"inputs": [{'e': 5, 'a': 5, 'd': 6}], "output": 4},
+                       {"inputs": [{'v': 4, 'x': 3, 'a': 7}], "output": 7},
+                   ]}
+        result = synthesize_data_transforms(
+            sys._getframe().f_code.co_name, [example], SynthesisSolver.Rosette, 60,
+            False)
         self.assertEqual(result[0]['solution'], '(unsat)')
 
-    def test4(self):
+    def test4cvc5(self):
         example = {"name": "f0",
                    "constraints": [
                        {"inputs": [{'lvl0': {'a': 4, 'b': 5, 'c': 6}}], "output": 4},
                        {"inputs": [{'e': 5, 'a': 5, 'd': 6}], "output": 5},
                        {"inputs": [{'v': 4, 'x': 3, 'a': 7}], "output": 7},
                    ]}
-        result = synthesize_data_transforms(sys._getframe().f_code.co_name, [example], 60,
+        result = synthesize_data_transforms(sys._getframe().f_code.co_name, [example], SynthesisSolver.CVC5, 60,
                                             False)
-        self.assertEqual(result[0]['solution'], '$[0]..a[0]')
+        self.assertEqual('$[0]..a[0]', result[0]['solution'])
+
+    def test4rosette(self):
+        example = {"name": "f0",
+                   "constraints": [
+                       {"inputs": [{'lvl0': {'a': 4, 'b': 5, 'c': 6}}], "output": 4},
+                       {"inputs": [{'e': 5, 'a': 5, 'd': 6}], "output": 5},
+                       {"inputs": [{'v': 4, 'x': 3, 'a': 7}], "output": 7},
+                   ]}
+        result = synthesize_data_transforms(sys._getframe().f_code.co_name, [example], SynthesisSolver.Rosette, 60,
+                                            False)
+        self.assertEqual('$[0]..a[0]', result[0]['solution'])
